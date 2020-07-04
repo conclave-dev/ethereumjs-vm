c5726e8a4ef5fee3d069feb79c9a273d6f73dab9
Khoa Phan <kp@conclave.dev>
2020-07-04 17:43:10 +0000

    Add getValidator gas price and check gas limit in numbervalidators precompile

---
+++ dist/evm/precompiles/f9-numbervalidators.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var utils = require('ethereumjs-util');
+var BN = utils.BN;
+var error = require('../../exceptions.js').ERROR;
+var assert = require('assert');
+
+module.exports = function (opts) {
+  assert(opts.data);
+
+  var results = {};
+  results.gasUsed = new BN(opts._common.param('gasPrices', 'getValidator'));
+  if (opts.gasLimit.lt(results.gasUsed)) {
+    results.return = Buffer.alloc(0);
+    results.gasUsed = opts.gasLimit;
+    results.exceptionError = error.OUT_OF_GAS;
+    results.exception = 0;
+    return results;
+  }
+};
